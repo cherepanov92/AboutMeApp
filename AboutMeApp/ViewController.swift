bd2f0bc3be8307c4c32b102e64e6382fc4a07694
Cherepanov Artyom <cherepanov92@gmail.com>
2025-03-09 23:55:06 +0500

    Initial Commit

---
+++ AboutMeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeApp
+//
+//  Created by User on 09.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
